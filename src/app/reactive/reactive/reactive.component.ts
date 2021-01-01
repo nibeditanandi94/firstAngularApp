41a8650a56f63e90d3c5b008a551eb396ce754e4
Nibedita Nandi <nibeditanandi1@gmail.com>
2021-01-01 17:02:42 -0600

    third commit

--- src/app/reactive/reactive/reactive.component.ts
+++ src/app/reactive/reactive/reactive.component.ts
@@ -10,20 +10,39 @@ import { Router } from '@angular/router';
 export class ReactiveComponent implements OnInit {
 genders=['male','female'];
 loginReactiveForm:FormGroup;
+forbiddenUserNames=['abc','xyz'];
   constructor(private router:Router) { }
 
   ngOnInit(): void {
     this.loginReactiveForm=new FormGroup({
       'userData': new FormGroup({
-        'username':new FormControl(null,Validators.required),
+        'username':new FormControl(null,[Validators.required,this.forbiddenName.bind(this)]),
         'email': new FormControl(null,[Validators.required,Validators.email]),
       }),
   
    'gender':new FormControl('female'),
-   'medicines': new FormArray([])
+   'medicines': new FormArray([]),
 
+   
+});
+//for checking the valuechanges in the form we will use observable i.e. listeners
+this.loginReactiveForm.statusChanges.subscribe(
+  (status)=>console.log(status)
+);
 
-    });
+this.loginReactiveForm.valueChanges.subscribe(
+(value) => console.log(value)
+);
+
+//prepopulating the form by setValues
+this.loginReactiveForm.setValue({
+  'userData':{
+    'username':"Nibedita",
+    'email':'nibedita@test.com',
+  },
+  'gender':'female',
+  'medicines':[]
+});
   }
   backbtn(){
     this.router.navigate(['/home']);
@@ -37,8 +56,24 @@ loginReactiveForm:FormGroup;
      getControls(){
        return (this.loginReactiveForm.get('medicines') as FormArray).controls
      }
+    
+     forbiddenName(control:FormControl):{[s:string]:boolean}{
+       //checking here value of the control is part of forbiddenNames array
+      if(this.forbiddenUserNames.indexOf(control.value)!== -1){
+        //checking control values matches the array , but returns -1 if
+        //not part of array. -1 is considered as true.
+        return {'nameisforbidden':true}
+      }
+      return null;
+     }
+     
+     
+
      submit(){
        console.log(this.loginReactiveForm);
+       //We can reset the form 
+       this.loginReactiveForm.reset();
      }
+
 
 }
